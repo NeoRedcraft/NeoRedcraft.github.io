5bf9ad429965c0fdb3f6d07ea0742115cc6f10e0
NeoRedcraft <155869707+NeoRedcraft@users.noreply.github.com>
2025-11-27 11:29:35 +0800

    feat: create initial Three.js portfolio page with interactive 3D scene and basic content structure.

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.152.2/examples/jsm/controls/OrbitControls.js';
 
 // Setup
 const scene = new THREE.Scene();
